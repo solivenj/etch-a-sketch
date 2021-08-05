4450106a5e89cb1e78cc926f87394f2911690b97
solivenj <soliven.j@husky.neu.edu>
2021-08-04 22:58:56 -0400

    Finished different color modes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // Global Variables
-const SIZE = 16;
+const STARTING_SIZE = 16;
+const STARTING_COLOR = "#f582ae";
+
+let size = STARTING_SIZE;
+let color = STARTING_COLOR;
+let mode = "color"; // mode is one of ("color", "grayscale", "rainbow")
+let colors = ['violet','indigo','blue','green','yellow','orange',
+                             'red'] // roygbiv colors
 
 function createGrid(size) {
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -16,7 +23,22 @@ function createGrid(size) {
 }
 
 function changeCellColor() {
-    this.style.backgroundColor = "red";
+    if (mode == "color") {
+        this.style.backgroundColor = color;
+    } else if (mode == "rainbow") {
+        this.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    } else if (mode == "grayscale") {
+        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1)); // get alpha value of color
+
+        if (currentOpacity < 1) {
+            this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+            console.log(this.style.backgroundColor);
+        } else if (currentOpacity > 1 && this.style.backgroundColor != `rgba(0, 0, 0, 1)`) {
+            this.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
+        }
+    } else if (mode == "eraser") {
+        this.style.backgroundColor = "#fef6e4";
+    }
 }
 
 function resetGrid() {
@@ -25,10 +47,42 @@ function resetGrid() {
 }
 
 function changeGridSize() {
-    size = slider.value;
-    gridSize.textContent = `${slider.value} x ${slider.value}`;
+    // size = slider.value;
     resetGrid();
     createGrid(size);
+}
+
+function updateGridSizeText() {
+    size = slider.value;
+    gridSize.textContent = `${size} x ${size}`;
+}
+function setColor() {
+    color = this.value;
+}
+
+function setMode(currentMode) {
+    // Remove active style from previousMode
+    if (mode == "color") {
+        colorButton.classList.remove("active");
+    } else if (mode=="grayscale") {
+        grayscaleButton.classList.remove("active");
+    }else if (mode == "rainbow") {
+        rainbowButton.classList.remove("active");
+    } else if(mode == "eraser") {
+        eraserButton.classList.remove("active");
+    }
+
+    mode = currentMode;
+
+    if (mode == "color") {
+        colorButton.classList.add("active");
+    } else if (mode=="grayscale") {
+        grayscaleButton.classList.add("active");
+    } else if (mode == "rainbow") {
+        rainbowButton.classList.add("active");
+    } else if(mode == "eraser") {
+        eraserButton.classList.add("active");
+    }
 }
 
 const grid = document.getElementById('grid');
@@ -36,11 +90,21 @@ const cells = grid.childNodes;
 const clearButton = document.getElementById("clear");
 const gridSize = document.getElementById("grid-size");
 const slider = document.getElementById("slider");
+const colorInput = document.getElementById("color-picker");
+const colorButton = document.getElementById("color-button");
+const grayscaleButton = document.getElementById("grayscale-button");
+const rainbowButton = document.getElementById("rainbow-button");
+const eraserButton = document.getElementById("eraser-button");
 
 clearButton.addEventListener("click", resetGrid);
-slider.addEventListener('input', changeGridSize);
+slider.addEventListener('input', updateGridSizeText);
+slider.addEventListener('change', changeGridSize);
+colorInput.addEventListener('input', setColor);
+
+colorButton.onclick = () => setMode('color');
+grayscaleButton.onclick = () => setMode('grayscale');
+rainbowButton.onclick = () => setMode('rainbow');
+eraserButton.onclick = () => setMode('eraser');
 
 
-
-let size = SIZE;
 createGrid(size);
